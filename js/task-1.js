5c5ac34a2c280d456527c74393bdd8b8edb4b564
OlexanderMotylgit config --global user.name OlexanderMotyl[200~git config --global user.name your_name00~git config --global user.name your_name ~git config --global user.name your_namegit config --global user.name your_namegit config --global user.name OlexanderMotylgit config --global user.email JustSanya1@gmail.com <JustSanya1@gmail.com>
2025-07-14 01:13:04 +0200

    hw

--- js/task-1.js
+++
@@ -1,6 +0,0 @@
-const categoriesList = document.querySelector("#categories");
-console.log(`Number of categories: ${categoriesList.childElementCount}`);
-for (const item of categoriesList.children) {
-    console.log(`Category: ${item.children[0].textContent}`);
-    console.log(`Elements: ${item.children[1].childElementCount}`);
-};
